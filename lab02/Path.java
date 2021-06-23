0663d00f9a5618fe4a944ae2451baa2ce5fec3e4
enor2017 <enor2017@163.com>
2021-06-24 05:24:20 +0800

    Finished lab2

--- lab02/Path.java
+++ lab02/Path.java
@@ -1,6 +1,40 @@
 /** A class that represents a path via pursuit curves. */
 public class Path {
+    private Point curr;
+    private Point next;
 
-    // TODO
+    public Path(double x, double y){
+        curr = new Point();
+        next = new Point(x, y);
+    }
+    public double getCurrX(){
+        return curr.getX();
+    }
 
+    public double getCurrY(){
+        return curr.getY();
+    }
+
+    public double getNextX(){
+        return next.getX();
+    }
+
+    public double getNextY(){
+        return next.getY();
+    }
+
+    public Point getCurrentPoint(){
+        return curr;
+    }
+
+    public void setCurrentPoint(Point point){
+        curr = point;
+    }
+
+    public void iterate(double dx, double dy){
+        curr.setX(next.getX());
+        curr.setY(next.getY());
+        next.setX(curr.getX() + dx);
+        next.setY(curr.getY() + dy);
+    }
 }
