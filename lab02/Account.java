a8033dc10308e74f70cc9dd0b5a791b76d826f88
enor2017 <enor2017@163.com>
2021-06-24 01:19:09 +0800

    Done with lab2 Account part.

--- lab02/Account.java
+++ lab02/Account.java
@@ -5,10 +5,17 @@
 public class Account {
 
     private int balance;
+    private Account parentAccount;
 
     /** Initialize an account with the given balance. */
     public Account(int balance) {
         this.balance = balance;
+        parentAccount = null;
+    }
+
+    public Account(int balance, Account parentAccount){
+        this.balance = balance;
+        this.parentAccount = parentAccount;
     }
 
     /** Returns the balance for the current account. */
@@ -30,14 +37,31 @@ public class Account {
      * would leave a negative balance, print an error message and leave the
      * balance unchanged.
      */
-    public void withdraw(int amount) {
-        // TODO
+    public boolean withdraw(int amount) {
         if (amount < 0) {
             System.out.println("Cannot withdraw negative amount.");
-        } else if (balance < amount) {
+            return false;
+        } else if (balance < amount && parentAccount == null) {
+            // balance insufficient, no parentAccount
             System.out.println("Insufficient funds");
+            return false;
+        } else if (balance < amount){
+            // balance insufficient, but has parentAccount
+            int overdraft = amount - balance;
+            if(parentAccount.withdraw(overdraft)){
+                // parentAccount can be overdrafted
+                // clear this balance
+                balance = 0;
+                return true;
+            } else {
+                // parentAccount balance insufficient
+                System.out.println("Insufficient funds for overdraft");
+                return false;
+            }
         } else {
+            // balance sufficient
             balance -= amount;
+            return true;
         }
     }
 
@@ -46,6 +70,9 @@ public class Account {
      * and depositing it into this account.
      */
     public void merge(Account other) {
-        // TODO
+        // cannot visit other.balance directly.
+        int otherBalance = other.getBalance();
+        balance += otherBalance;
+        other.withdraw(otherBalance);
     }
 }
