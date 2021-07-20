f81a87128a3d25fa6d5ffa36cd2c1e52bc39e52b
enor2017 <enor2017@163.com>
2021-07-21 06:32:32 +0800

    Done with lab13

--- lab13/BinarySearchTree.java
+++ lab13/BinarySearchTree.java
@@ -1,24 +1,88 @@
+import java.util.ArrayList;
+
 public class BinarySearchTree<T extends Comparable<T>> extends BinaryTree<T> {
 
     /* Creates an empty BST. */
     public BinarySearchTree() {
-        // TODO: YOUR CODE HERE
+        super();
     }
 
     /* Creates a BST with root as ROOT. */
     public BinarySearchTree(TreeNode root) {
-        // TODO: YOUR CODE HERE
+        super(root);
+    }
+
+    /* helper function: Array slicing, return subArray of [start, end) */
+    private ArrayList<T> slicing (ArrayList<T> array, int start, int end) {
+        ArrayList<T> subArray = new ArrayList<>();
+        for(int i = start; i < end; ++i) {
+            subArray.add(array.get(i));
+        }
+        return subArray;
+    }
+
+    /* helper function: return a tree node with given preorder and inorder traversal */
+    private TreeNode traversalConstruct(ArrayList<T> pre, ArrayList<T> in) {
+        if (pre.isEmpty()) {
+            return null;
+        }
+        // find root node
+        TreeNode root = new TreeNode(pre.get(0));
+        // if length of pre, in is 1, return (no children)
+        if (pre.size() == 1) {
+            return root;
+        }
+
+        // get root node index in inOrder list
+        int rootIndex = in.indexOf(root.item);
+        // recursively construct left and right subtree
+        root.left = traversalConstruct(slicing(pre, 1, 1 + rootIndex),
+                slicing(in, 0, rootIndex));
+        root.right = traversalConstruct(slicing(pre, 1 + rootIndex, pre.size()),
+                slicing(in, 1 + rootIndex, in.size()));
+        return root;
+    }
+
+    /* Creates a BST with preorder and inorder traversal */
+    public BinarySearchTree(ArrayList<T> pre, ArrayList<T> in) {
+        root = traversalConstruct(pre, in);
+    }
+
+    /* helper method: return true if given subtree contains given key */
+    private boolean contains(TreeNode t, T key) {
+        if (t == null) {
+            return false;
+        }
+        if (key.compareTo(t.item) == 0) {
+            return true;
+        } else if (key.compareTo(t.item) < 0) {
+            return contains(t.left, key);
+        } else {
+            return contains(t.right, key);
+        }
     }
 
     /* Returns true if the BST contains the given KEY. */
     public boolean contains(T key) {
-        // TODO: YOUR CODE HERE
-        return false;
+        return contains(root, key);
+    }
+
+    /* Add a node for KEY in the given subtree, return the root */
+    private TreeNode add(TreeNode t, T key) {
+        if (t == null) {
+            return new TreeNode(key);
+        }
+        if (key.compareTo(t.item) < 0) {
+            t.left = add(t.left, key);
+        } else if (key.compareTo(t.item) > 0) {
+            t.right = add(t.right, key);
+        }
+        return t;
     }
 
     /* Adds a node for KEY iff KEY isn't in the BST already. */
     public void add(T key) {
-        // TODO: YOUR CODE HERE
+        root = add(root, key);
     }
 
     /* Deletes a node from the BST. 
--- lab13/BinaryTreeTest.java
+++ lab13/BinaryTreeTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 public class BinaryTreeTest {
     @Test
@@ -19,5 +20,75 @@ public class BinaryTreeTest {
         System.setOut(oldOut);
         assertEquals("x in preorder\nC A B E D \nx in inorder\nA B C D E \n\n".trim(),
                      outContent.toString().trim());
+    }
+
+    @Test
+    public void traversalConstructor() {
+        ArrayList<String> preOrder = new ArrayList<>();
+        preOrder.add("C");
+        preOrder.add("A");
+        preOrder.add("B");
+        preOrder.add("E");
+        preOrder.add("D");
+        ArrayList<String> inOrder = new ArrayList<>();
+        inOrder.add("A");
+        inOrder.add("B");
+        inOrder.add("C");
+        inOrder.add("D");
+        inOrder.add("E");
+        BinarySearchTree<String> x = new BinarySearchTree<>(preOrder, inOrder);
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream oldOut = System.out;
+        System.setOut(new PrintStream(outContent));
+        BinaryTree.print(x, "x");
+        System.setOut(oldOut);
+        assertEquals("x in preorder\nC A B E D \nx in inorder\nA B C D E \n\n".trim(),
+                outContent.toString().trim());
+    }
+
+    @Test
+    public void containEdgeTest() {
+        BinarySearchTree<String> x = new BinarySearchTree<String>();
+
+        assertFalse(x.contains("A"));
+        assertFalse(x.contains(" "));
+        assertFalse(x.contains(""));
+
+        x.add("A");
+
+        assertTrue(x.contains("A"));
+        assertFalse(x.contains(" "));
+        assertFalse(x.contains(""));
+
+        x.add("B");
+        x.add("C");
+        x.add("D");
+        x.add("E");
+
+        assertTrue(x.contains("A"));
+        assertFalse(x.contains(" "));
+        assertFalse(x.contains(""));
+        assertTrue(x.contains("B"));
+        assertTrue(x.contains("C"));
+        assertTrue(x.contains("D"));
+        assertTrue(x.contains("E"));
+    }
+
+    @Test
+    public void containsTest() {
+        BinarySearchTree<String> x = new BinarySearchTree<String>();
+        x.add("C");
+        x.add("A");
+        x.add("E");
+        x.add("B");
+        x.add("D");
+
+        assertTrue(x.contains("A"));
+        assertTrue(x.contains("B"));
+        assertTrue(x.contains("C"));
+        assertTrue(x.contains("D"));
+        assertTrue(x.contains("E"));
+        assertFalse(x.contains("F"));
+        assertFalse(x.contains(" "));
     }
 }
